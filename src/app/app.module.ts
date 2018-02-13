2d1a2f63233d57b1203dfdc1db917970ec794c85
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2018-02-13 16:48:11 +0100

    create dashboard template and basic routing
    Dashboard layout was created with basic color scheme and alignment.
    Basic routing was implemented. Default route was created leads to authentication component and dashboard route leads to  the dashboard component.
    
    FINISHED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -10,11 +12,16 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [
     AppComponent,
     AuthenticationComponent,
+    DashboardComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AuthenticationComponent },
+      { path: 'dashboard', component: DashboardComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
