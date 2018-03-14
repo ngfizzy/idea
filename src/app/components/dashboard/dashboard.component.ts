132582219ce643d0a462430fceb048cbe12a3db7
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2018-03-14 21:19:46 +0100

    implement logout(auth):
    A dropdown menu was added to the header for showing options whenever user icon is being hover over. A logout method was implemented in user service which deletes authToken  from local storge
    
    FINISHES LOGOUT TASK

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,37 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  isUserOptionsVisible = false;
+  constructor (private userService: UserService) {
+  }
+
   ngOnInit() {}
+
+  /**
+   * Shows users options whenever you hover over user icon
+   *
+   * @returns {void}
+   */
+  showUserOptions(): void {
+    this.isUserOptionsVisible = true;
+  }
+
+  /**
+   * Hides options whenever you hover over the user data
+   *
+   * @returns {void}
+   */
+  hideUserOptions(): void {
+    this.isUserOptionsVisible = false;
+  }
+
+  /**
+   * It logs user out
+   *
+   * @returns {void}
+   */
+  logout(): void {
+    this.userService.logout();
+  }
+
 }
