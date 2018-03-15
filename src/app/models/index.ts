1bcb19e9d0bda48b2d75d2e170d1d76f784f1efa
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2018-03-15 17:26:56 +0100

    implement note createion(note):
    A note editor was created used for creating note. A new method was also created in note service used to createthat makes the actuall http request.
    
    FINISH CREATE NOTE

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,1 +1,2 @@
 export { User } from './user.interface';
+export { Note } from './note.interface';
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/delay';
 
 import { Observable } from 'rxjs/Rx';
 import { Http, Response, Headers } from '@angular/http';
 
 import { apiBaseUrl } from '../../env';
+import { Note } from '../models';
 
 @Injectable()
 export class NoteService {
   private status: number;
 
-  private posts = [];
+  private posts: Array<any>;
 
   constructor(private http: Http) {
+    this.posts = [];
   }
 
   /**
@@ -30,8 +31,10 @@ export class NoteService {
     return this.http.get(url, { headers })
       .map((response: Response) => {
         this.status = response.status;
-        this.posts = response.json();
-      const result =  {
+        this.posts = Array
+          .from(response.json().notes)
+          .reverse();
+        const result = {
           posts: this.posts,
           status: this.status,
         };
@@ -63,4 +66,53 @@ export class NoteService {
     return this.status;
   }
 
+  /**
+   * It creates a note
+   *
+   * @param {Note} note a note
+   *
+   * @returns {Observable<any>} It returns an observable of created notes
+   */
+  createNote(note: Note): Observable<any> {
+    const url = `${apiBaseUrl}/notes`;
+    const headers =
+      new Headers({ authorization: localStorage.getItem('authToken') });
+
+    return this.http.post(url, note, { headers })
+      .map((response: Response) => {
+        this.posts.unshift(response.json().note);
+        return 'Note Saved';
+      })
+      .catch(this.handleCreateError.bind(this));
+  }
+
+
+  /**
+   * It throws and observable of errorMessage
+   *
+   * @param response User
+   *
+   * @returns {Observable<any>} Observable of error  messsage
+   */
+  handleCreateError(response: Response): Observable<any> {
+    if (response.status === 500) {
+      return Observable.throw('We are sorry. An error occurred while trying to create this note');
+    }
+
+    const errorMessage = this.getCreateMessage(response.json());
+
+    return Observable.throw(errorMessage);
+  }
+
+  /**
+   * It extract error message from error object
+   *
+   * @param {object} errors error object
+   *
+   * @returns {string} last error message on the list of error messages
+   */
+  private getCreateMessage(errors) {
+    const fields = Object.keys(errors);
+    return errors[fields.shift()].pop();
+  }
 }
