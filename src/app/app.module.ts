ddbe5ff53368a3cf853631dc99fe4e53ecc7f51b
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2018-04-08 04:55:05 +0100

    animate-auth(animation)
    All authentication components were animated in this commit. Login and signup in and out of the view. Input fields expands on focus.
    
    FINISHES ANIMATE AUTH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
@@ -38,6 +39,7 @@ import { PasswordResetService } from './services/password-reset.service';
     PasswordResetRequestFormComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpModule,
     FormsModule,
